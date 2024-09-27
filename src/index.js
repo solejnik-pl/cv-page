9b1692842d6ca96ae1f8cf9dcfa58ecc5236856e
Szymon Olejnik <szymon@solejnik.pl>
2024-09-27 21:02:25 +0200

    added generating german part of the page

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ const __dirname = path.resolve();
 const template = Handlebars.compile(fs.readFileSync(__dirname + '/src/template/template.html', 'utf8'));
 
 const data = {
-    en: JSON.parse(fs.readFileSync(__dirname + '/src/assets/lang/en.json', 'utf8')),
-    de: JSON.parse(fs.readFileSync(__dirname + '/src/assets/lang/de.json', 'utf8')),
+    languages: [{
+        data: JSON.parse(fs.readFileSync(__dirname + '/src/assets/lang/en.json', 'utf8'))
+    },{
+        data: JSON.parse(fs.readFileSync(__dirname + '/src/assets/lang/de.json', 'utf8'))
+    }]
 };
 
 const output = template(data);
