6602420ef859e67309cc246a4f3cd954d2408d69
Szymon Olejnik <szymon@solejnik.pl>
2024-09-27 16:24:59 +0200

    added generating static page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import Handlebars from "handlebars";
-const template = Handlebars.compile("Name: {{name}}");
-console.log(template({ name: "Nils" }));
+import Handlebars from 'handlebars';
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+
+const template = Handlebars.compile(fs.readFileSync(__dirname + '/src/template/template.html', 'utf8'));
+
+const data = {
+    en: JSON.parse(fs.readFileSync(__dirname + '/src/assets/lang/en.json', 'utf8')),
+    de: JSON.parse(fs.readFileSync(__dirname + '/src/assets/lang/de.json', 'utf8')),
+};
+
+const output = template(data);
+console.log(output);
